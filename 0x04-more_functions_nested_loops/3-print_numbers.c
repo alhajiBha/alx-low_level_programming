16ad1dcc0f641a8173e7da729871e480ff94fd97
Sahad <alhajibha20@gmail.com>
2023-07-20 10:29:00 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: Prints the number with _putcher
+ *
+ * Return: void
+ */
+
+void print_number(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
