16b200b5351d3e4a62866c8ec3f464dae5bfde80
Sahad <alhajibha20@gmail.com>
2023-07-24 02:33:52 -0700

    reset_to_98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef INV_HOLBERTON_H
 #define INV_HOLBERTON_H
-/**
 
 void reset_to_98(int *n);
 
@@ -24,8 +23,3 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 #endif
-*/
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
